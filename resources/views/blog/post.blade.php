3f5c3fe6a720538a79b305e39ecab9913451d9f8
Daniel Morgan <me@danielmorgan.co.uk>
2017-01-31 14:12:14 +0000

    Initial commit

---
+++ resources/views/blog/post.blade.php
@@ -0,0 +1,19 @@
+@extends('layouts.base')
+
+@section('content')
+    <div class="Title">
+        <h1 class="Title__text">Hello world!</h1>
+    </div>
+
+    <article class="Post">
+        <div class="Post__lead">
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. A accusamus, ad consequatur consequuntur debitis dolor ea est impedit incidunt iste maxime nesciunt quaerat, quia quis rem rerum, suscipit tempora temporibus!</p>
+        </div>
+
+        <div class="Post__body">
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. A accusamus, ad consequatur consequuntur debitis dolor ea est impedit incidunt iste maxime nesciunt quaerat, quia quis rem rerum, suscipit tempora temporibus!</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. A accusamus, ad consequatur consequuntur debitis dolor ea est impedit incidunt iste maxime nesciunt quaerat, quia quis rem rerum, suscipit tempora temporibus!</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. A accusamus, ad consequatur consequuntur debitis dolor ea est impedit incidunt iste maxime nesciunt quaerat, quia quis rem rerum, suscipit tempora temporibus!</p>
+        </div>
+    </article>
+@endsection
---
+++ resources/views/layouts/base.blade.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title></title>
+
+    <link href="{{ mix('/css/app.css') }}" rel="stylesheet">
+    @stack('styles')
+</head>
+<body>
+    <div class="Container">
+        <div class="Content">
+            @yield('content')
+        </div>
+
+        <aside class="Sidebar">
+            <section class="Widget">
+                <h3 class="Widget__title">Latest Posts</h3>
+                <ul class="Widget__content">
+                    <li>Lorem ipsum</li>
+                    <li>Dolor sit amet</li>
+                    <li>Consectetur</li>
+                    <li>Adisciping</li>
+                    <li>Lost grid</li>
+                    <li>Lookie here</li>
+                </ul>
+            </section>
+            <section class="Widget">
+                <h3 class="Widget__title">About Me</h3>
+                <p class="Widget__content">Lorem ipsum dolor sit amet, consectetur adipisicing elit. A ad, quod! Alias distinctio ea et hic inventore libero.</p>
+            </section>
+        </aside>
+    </div>
+
+    @stack('scripts')
+
+    <link href="https://fonts.googleapis.com/css?family=Playfair+Display" rel="stylesheet">
+</body>
+</html>
